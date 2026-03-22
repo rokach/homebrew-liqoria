99ea09e71a4effa0344169ba0a768db56b108b9e
Aviorrok <aviorrok@gmail.com>
2026-03-23 00:23:18 +0200

    Update liqoria.rb

--- Casks/liqoria.rb
+++ Casks/liqoria.rb
@@ -2,8 +2,8 @@ cask "liqoria" do
   version "1.2.0"
   sha256 "c4bd30ddb16fd3119516bd1164d9a37f1636a19d9febbd93f3483b2058e513b7"
 
-  url "https://github.com/aviorrok/homebrew-liqoria/releases/download/v#{version}/Liqoria.zip",
-      verified: "github.com/aviorrok/homebrew-liqoria/"
+  url "https://github.com/rokach/homebrew-liqoria/releases/download/v#{version}/Liqoria.zip",
+    verified: "github.com/rokach/homebrew-liqoria/"
 
   name "Liqoria"
   desc "Mac music player - Apple Music Alternative, Spotify Alternative, YouTube, and more"
